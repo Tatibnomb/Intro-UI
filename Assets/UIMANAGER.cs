fea0f39f51bde0ea4fdaf2dcf4bb690d4de31432
Tatibnomb <49379010@est.ort.edu.ar>
2025-04-21 10:41:24 -0300

    Saludo terminado

---
+++ Assets/UIMANAGER.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIMANAGER : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+    public TMP_InputField inputNombre;
+    string nombreIngresado;
+    string saludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        // Obtener el nombre del inputfield
+
+        nombreIngresado = inputNombre.text;
+
+        // Concatenar el nombre con un saludo
+
+        saludo = "Hola " + nombreIngresado + "!";
+
+        // Mostrar el saludo en txtSaludo
+
+        txtSaludo.text = saludo;
+    }
+}
