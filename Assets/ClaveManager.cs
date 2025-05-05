fb9ef705baa83b0ac91223f31d229b1ce05d19e9
Tatibnomb <49379010@est.ort.edu.ar>
2025-05-05 10:42:19 -0300

    EAC terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputClave;
+    string claveIngresada;
+    string clave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = " ";
+        clave = "Tati";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IngresarClave()
+    {
+
+        // Concatenar la clave con un mensaje diciendo si la clave es correcta o incorrecta
+
+        claveIngresada = inputClave.text;
+        if (claveIngresada == clave)
+        {
+            txtResultado.text = "Clave correcta";
+        }
+        else
+        {
+            txtResultado.text = "Clave incorrecta";
+        }
+    }
+}
